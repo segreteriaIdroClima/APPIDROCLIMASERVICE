8685e8d8a1709e933df0a37ccf2f50eb9d5f8fbf
segreteriaIdroClima <segreteria@idroclimaservicemilano.it>
2026-04-04 15:05:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = 'portale-aziendale-v1';
+const CACHE_NAME = 'portale-aziendale-v2';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
     './style.css',
     './app.js',
     './manifest.json',
+    './idroclima-app-192.png',
+    './idroclima-drop-48.png',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
     'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap'
 ];
